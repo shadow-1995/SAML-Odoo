bde23cb882fdd0040d1eb6e0ba70a732c4a8fde0
divy9t <divy9t12@icloud.com>
2019-04-01 06:31:08 +0000

    Change of Fields

--- ticket_system/models/teams.py
+++ ticket_system/models/teams.py
@@ -5,8 +5,6 @@ class ticket_system_teams(models.Model):
     
     name = fields.Char()
     description = fields.Text()
-    member_ids = fields.Many2Many('res.users',string="Team Members")
+    member_ids = fields.Many2many('res.users',string="Team Members")
     assignment_method = fields.Selection([('Balanced','Balanced'),('Manual','Manual'),('Random','Random')],string="Assignment Method",default='Manual')
     use_alias = fields.Boolean(string="Email Alias",default=True)
-    priority = field.Selection([('Low','Low'),('High','High'),('Main Priority','Main Priority')],string='Team Members')
-    
