a8e73711b9f9bd225568d1565f8a71f05f3ddb04
nitin thakur <nitinthakur75@gmail.com>
2019-07-31 08:35:17 +0000

    new module

---
+++ student/.ipynb_checkpoints/__manifest__-checkpoint.py
@@ -0,0 +1,34 @@
+# -*- coding: utf-8 -*-
+{
+    'name': "Student Record",
+
+    'summary': """This module will add a record to store student details""",
+
+    'description': """
+        This module will add a record to store student details
+        
+    """,
+
+    'author': "My Company",
+    'website': "http://www.yourcompany.com",
+
+    # Categories can be used to filter modules in modules listing
+    # Check https://github.com/odoo/odoo/blob/12.0/odoo/addons/base/data/ir_module_category_data.xml
+    # for the full list
+    'category': 'Tools',
+    'version': '1.0',
+
+    # any module necessary for this one to work correctly
+    'depends': ['base'],
+
+    # always loaded
+    'data': [
+        'security/ir.model.access.csv',
+        'views/views.xml',
+        'views/templates.xml',
+    ],
+    # only loaded in demonstration mode
+    'demo': [
+        'demo/demo.xml',
+    ],
+}
